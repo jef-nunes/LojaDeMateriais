6b1ef281a9a9090bd07d31454a3e98db4c9243df
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2024-11-20 16:29:07 -0300

    Create insere_vendas.sql

---
+++ insere_vendas.sql
@@ -0,0 +1,23 @@
+INSERT INTO venda (venda_id, valor_total, fk_produto_id, fk_produto_categoria_produto_id, fk_produto_fornecedor_id, fk_cliente_id) 
+VALUES
+(1, 45.99, 1, 1, 1, 1), -- Joao Silva compra Tinta Acrílica Branca
+(2, 38.50, 2, 1, 3, 2), -- Maria Souza compra Tinta Látex Azul
+(3, 29.99, 3, 1, 1, 3), -- Ana Oliveira compra Tinta Esmalte Sintético
+(4, 19.90, 4, 1, 1, 4), -- Carlos Santos compra Tinta Spray Vermelha
+(5, 22.75, 5, 1, 1, 5), -- Lucas Costa compra Tinta PVA Branco
+(6, 99.50, 6, 1, 3, 6), -- José Silva compra Tinta Epóxi Verde
+(7, 120.00, 7, 1, 1, 7), -- Gustavo Rocha compra Tinta Automotiva Preta
+(8, 55.00, 8, 1, 1, 8), -- José Santos compra Tinta à Base de Água
+(9, 69.80, 9, 1, 6, 9), -- Juliana Lima compra Tinta Acrílica Lavável
+(10, 79.99, 10, 1, 1, 10), -- Ricardo Martins compra Tinta Térmica
+
+(11, 19.90, 11, 2, 1, 11), -- Claudia Rodrigues compra Martelo
+(12, 15.00, 12, 2, 1, 12), -- Roberto Ferreira compra Chave de Fenda 6mm
+(13, 22.50, 13, 2, 1, 13), -- Leandro Silva compra Alicate Universal
+(14, 180.00, 14, 2, 7, 14), -- Bruno Pereira compra Serra Tico-Tico Elétrica
+(15, 35.00, 15, 2, 3, 15), -- Ricardo Almeida compra Chave Inglesa 12 polegadas
+(16, 12.00, 16, 2, 4, 16), -- João Santos compra Lima de Ferro
+(17, 220.00, 17, 2, 5, 17), -- Eduardo Costa compra Furadeira Elétrica 500W
+(18, 80.00, 18, 2, 1, 18), -- Luiz Souza compra Plaina Manual
+(19, 25.00, 19, 2, 1, 19), -- Raquel Lima compra Régua de Alumínio
+(20, 35.99, 20, 2, 2, 20); -- João Santos compra Trena 5m
