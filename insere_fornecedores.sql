4ad38a99859ed44388ae11b13cee286f77a2aade
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2024-11-20 16:49:45 -0300

    Update insere_fornecedores.sql

--- insere_fornecedores.sql
+++ insere_fornecedores.sql
@@ -1,11 +1,12 @@
+USE `LojaDeMateriais`;
 INSERT INTO `LojaDeMateriais`.`fornecedor` (`nome`, `email`, `telefone`, `estado`, `cidade`) VALUES
-('Construmix Materiais', 'contato@construmix.com.br', '11-98765-4321', 'SP', 'São Paulo'),
-('Obra Total', 'vendas@obratotal.com.br', '11-97654-3210', 'RJ', 'Rio de Janeiro'),
-('Fundação Forte', 'atendimento@fundacaoforte.com.br', '21-99999-8888', 'MG', 'Belo Horizonte'),
-('Materiais RS', 'comercial@materiaisrs.com.br', '31-98888-7777', 'RS', 'Porto Alegre'),
-('ConstruCenter', 'suporte@construcenter.com.br', '41-97777-6666', 'PR', 'Curitiba'),
+('Construmix Materiais', 'contato@construmix.com.br', '11987654321', 'SP', 'São Paulo'),
+('Obra Total', 'vendas@obratotal.com.br', '11976543210', 'RJ', 'Rio de Janeiro'),
+('Fundação Forte', 'atendimento@fundacaoforte.com.br', '21999998888', 'MG', 'Belo Horizonte'),
+('Materiais RS', 'comercial@materiaisrs.com.br', '31988887777', 'RS', 'Porto Alegre'),
+('ConstruCenter', 'suporte@construcenter.com.br', '41977776666', 'PR', 'Curitiba'),
 ('Fornecedora Salvador', 'fornecedora@salvadormateriais.com.br', '51-96666-5555', 'BA', 'Salvador'),
-('Construfácil', 'contato@construfacil.com.br', '61-95555-4444', 'DF', 'Brasília'),
-('Obras Nordeste', 'obrasnordeste@obrasnordeste.com.br', '71-94444-3333', 'PE', 'Recife'),
-('ConstruNova', 'vendas@construnova.com.br', '81-93333-2222', 'CE', 'Fortaleza'),
-('Materiais Amazonia', 'contato@materiaisamazon.com.br', '91-92222-1111', 'AM', 'Manaus');
+('Construfácil', 'contato@construfacil.com.br', '61955554444', 'DF', 'Brasília'),
+('Obras Nordeste', 'obrasnordeste@obrasnordeste.com.br', '71944443333', 'PE', 'Recife'),
+('ConstruNova', 'vendas@construnova.com.br', '81933332222', 'CE', 'Fortaleza'),
+('Materiais Amazonia', 'contato@materiaisamazon.com.br', '91922221111', 'AM', 'Manaus');
