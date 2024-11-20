54797114f441630afedfef4d5efe050179b905c2
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2024-11-20 16:05:10 -0300

    Create insere_categoria_produto.sql

---
+++ insere_categoria_produto.sql
@@ -0,0 +1,7 @@
+INSERT INTO categoria_produto(nome, descricao, codigo)
+VALUES
+("Tintas", "Produtos destinados à pintura e acabamento", "C01"),
+("Ferramentas", "Instrumentos para construção e manutenção", "C02"),
+("Materiais", "Produtos diversos para construção e reforma", "C03"),
+("Eletrica", "Produtos e materiais para instalações elétricas", "C04"),
+("Hidraulica", "Materiais para instalações hidráulicas e encanamento", "C05");
